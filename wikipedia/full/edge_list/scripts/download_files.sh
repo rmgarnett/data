75182c39288a5852a497150e9c06f183414a0b1d
rmgarnett <romanempire@gmail.com>
2012-02-13 00:54:24 -0500

    wikipedia edge list

--- wikipedia/full/edge_list/scripts/download_files.sh
+++ wikipedia/full/edge_list/scripts/download_files.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 PREFIX='https://s3.amazonaws.com/machine_learning_data/wikipedia/edge_list/data/'
-DATA_DIRECTORY='../data/'
+DATA_DIRECTORY='../raw/'
 
 for file in $(cat file_list)
 do
